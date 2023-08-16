16eeb5c649242dde25889c8e4b280ec1a046fd71
jiyeon0113 <jeny51264520@gmail.com>
2023-08-16 11:39:58 +0900

    update for main

--- src/screens/screensSetting/SoftwareLicense.js
+++ src/screens/screensSetting/SoftwareLicense.js
@@ -10,6 +10,16 @@ const SoftwareLicense = () => {
         navigation.goBack();
     };
 
+    const licenses = [
+        { library: 'React Native', license: 'MIT License' },
+        { library: 'React Navigation', license: 'BSD License' },
+        { library: 'Redux', license: 'MIT License' },
+        { library: 'Axios', license: 'MIT License' },
+        { library: 'Lodash', license: 'MIT License' },
+        { library: 'AsyncStorage', license: 'Apache License 2.0' },
+        // 더 많은 라이브러리 정보 추가
+    ];
+
     return (
         <View style={styles.container}>
             {/* 상단 바 */}
@@ -22,43 +32,15 @@ const SoftwareLicense = () => {
 
             {/* 이용 약관 내용 */}
             <ScrollView contentContainerStyle={styles.scrollViewContent}>
-            <View style={styles.contentContainer}>
-                <Text style={styles.title}>소프트웨어 라이선스</Text>
-                <Text style={styles.content}>
-                제 1조 (정의){"\n"}
-                본 소프트웨어 라이선스(이하 '라이선스')는 [소프트웨어 이름] (이하 '소프트웨어')의 사용 조건과 제한 사항을 규정합니다.{"\n"}
-                {"\n"}
-                제 2조 (라이선스 부여){"\n"}
-                {"\n"}
-                1. 소프트웨어의 라이선스는 사용자에게 무상으로 부여되며, 사용자는 소프트웨어를 사용할 수 있는 권한을 얻습니다.{"\n"}
-                2. 라이선스는 개인 및 기업을 포함한 모든 사용자에게 부여됩니다.{"\n"}
-                {"\n"}
-                제 3조 (사용 권한){"\n"}
-                {"\n"}
-                1. 사용자는 소프트웨어를 개인적인 목적으로 무제한 사용할 수 있습니다.{"\n"}
-                2. 사용자는 소프트웨어를 수정, 복제, 배포할 수 있습니다.{"\n"}
-                3. 단, 사용자는 소프트웨어를 상업적인 목적으로 사용하거나 판매할 경우 별도의 라이선스 동의를 받아야 합니다.{"\n"}
-                {"\n"}
-                제 4조 (책임 제한){"\n"}
-                {"\n"}
-                1. 소프트웨어는 "있는 그대로" 제공되며, 어떠한 경우에도 제작자는 소프트웨어의 정확성, 신뢰성, 완전성 등에 대한 어떠한 책임도 지지 않습니다.{"\n"}
-                2. 어떠한 손해도 제작자는 책임을 지지 않습니다.{"\n"}
-                {"\n"}
-                제 5조 (라이선스의 해지){"\n"}
-                {"\n"}
-                1. 사용자는 언제든지 라이선스를 해지할 수 있습니다.{"\n"}
-                2. 라이선스가 해지된 경우, 사용자는 소프트웨어를 더 이상 사용할 수 없습니다.{"\n"}
-                {"\n"}
-                제 6조 (변경 및 업데이트){"\n"}
-                {"\n"}
-                1. 제작자는 소프트웨어의 변경 및 업데이트를 원하는 경우 언제든지 가능하며, 변경된 소프트웨어 역시 해당 라이선스에 따라 제공됩니다.{"\n"}
-                제 7조 (분쟁해결){"\n"}
-                {"\n"}
-                본 라이선스에서 정하지 않은 사항 및 본 라이선스의 해석에 관한 분쟁은 관련 법령에 따라 합의 또는 조정을 시도하며, 해결되지 않는 경우 관할 법원에 소를 제기할 수 있습니다.{"\n"}
-                {"\n"}
-                이는 간단한 예시일 뿐이며, 실제 소프트웨어의 특성과 목적에 맞게 라이선스를 작성하셔야 합니다. 상세한 내용 및 법적 효력을 확인하기 위해서는 변호사나 전문가와 상담하시기를 권장합니다.
-                </Text>
-            </View>
+                <View style={styles.contentContainer}>
+                    <Text style={styles.title}>소프트웨어 라이선스</Text>
+                    {licenses.map((item, index) => (
+                        <View key={index} style={styles.licenseItem}>
+                            <Text style={styles.library}>{item.library}</Text>
+                            <Text style={styles.license}>{item.license}</Text>
+                        </View>
+                    ))}
+                </View>
             </ScrollView>
         </View>
     );
@@ -95,9 +77,19 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginBottom: 10,
     },
-    content: {
+    licenseItem: {
+        flexDirection: 'row',  // 세로 정렬을 위해 가로로 나열
+        marginBottom: 10,
+    },
+    library: {
+        flex: 1,  // 공간을 고르게 분할하여 세로 정렬
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    license: {
+        flex: 1,  // 공간을 고르게 분할하여 세로 정렬
         fontSize: 16,
-        lineHeight: 24,
+        color: 'gray',
     },
 });
 
